ef958863b574b3d629206dda0d63bfab5a413221
swisszz <snookerjack@gmail.com>
2025-12-21 20:08:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -191,11 +191,9 @@ def playlist():
 
 @app.route("/")
 def home():
-    return "Live TV M3U Server is running"
-
+    return "Live TV Stream Proxy is running"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
 
-    
-   
